c067a397c92c7f4c1cf3d5b412bc95ed55f4380d
dominikpeuscher <dpeuscher@gmail.com>
2014-01-12 19:32:35 +0100

    New syntax for family classes Case-sensitive call to algebra in use case corrected

--- sugarJExpressionProblem/src/usage/Test.java
+++ sugarJExpressionProblem/src/usage/Test.java
@@ -7,8 +7,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		IntAlgSubCount.Methods expr = 
-			IntAlgSubCount.Generator().sub(IntAlgSubCount.Generator().lit(3), 
-					IntAlgSubCount.Generator().lit(1));
+			IntAlgSubCount.Algebra().Sub(IntAlgSubCount.Algebra().Lit(3), 
+					IntAlgSubCount.Algebra().Lit(1));
 		System.out.println(expr.print("Expression: ") + " has " +
 				expr.count().toString() + " Operators");
 	}
